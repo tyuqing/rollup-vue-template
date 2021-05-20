11d28f0ee7871d1671ef3099f11a1091c0ad9da6
tyuqing <tyuqing7@163.com>
2021-05-20 18:05:08 +0800

    chore: 资源文件换一个文件夹

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,21 @@ import vuePlugin from 'rollup-plugin-vue'
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs'
 import replacePlugin from 'rollup-plugin-replace'
+import copyPlugin from 'rollup-plugin-copy'
+
 export default {
   input: 'src/main.js',
   output: {
-    file: 'src/.temp/browser.js',
+    file: 'dist/browser.js',
     format: 'iife',
   },
   plugins: [
+    copyPlugin({
+      targets:[{ src: 'public/*', dest: 'dist' }]
+    }),
     resolve(),
     vuePlugin({
+      css: true,
       compileTemplate: true,
       target: 'browser',
     }),
